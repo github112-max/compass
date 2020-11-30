c94fd72f0f373a8b6800eec11f0bc3d67d282374
github112-max <75213040+github112-max@users.noreply.github.com>
2020-11-30 22:24:01 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let degrees = 0
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45 || degrees > 315) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else {
+        basic.showString("W")
+    }
+})
